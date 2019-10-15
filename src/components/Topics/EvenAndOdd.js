afc96015766aa49fe23d30036009c8358c27a74c
Nate Roundy <nroundy801@gmail.com>
2019-10-14 18:15:30 -0600

    Still woking on project

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export default class EvenAndOdd extends Component {
+  constructor() {
+    super()
+  }
+
+  render(){
+    return (
+      <p>Even And Odd Component</p>
+    )
+  }
+}
---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class FilterObject extends Component {
+  constructor(){
+    super()
+  }
+
+  render() {
+    return(
+      <p>Filter Object Component</p>
+    )
+    
+  }
+}
